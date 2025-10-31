60ebaa472687b0a67ad61659a686e6c80944d861
bia224 <bia224@lehigh.edu>
2025-10-31 18:09:53 -0400

    Made the function that handles Rook, Knight, King, Bishop and Queen. Previous code could handle all these but the name was different.

---
+++ valid_moves/linear_jump_step_move.py
@@ -0,0 +1,38 @@
+def get_linear_moves(self, position, directions, color):
+
+    """ This function is for linear movement of chess pieces 
+        (e.g. Queen, Rook, and Bishop). The move can be straight
+        or diagonal depending on the directions argument.
+
+        It can also be jump moves for the Knight,
+        and ones-step move for King. It allows forward-backward moves.
+        Not applicable to Pawns.
+        
+        Parameters:
+                position: Get current position of the chess piece.
+                directions: Gives potential moving direction of chess piece.
+                color: Get information whether piece is Black or White.
+        Output:
+                moves: Possible moves for the chess piece.
+                       Move may be invalid when King is in check.
+    """
+
+    moves = [] # initialize a list for possible moves
+
+    for dx, dy in directions: # gets (x, y) coordinates from the directions
+        x, y = position # this is position of chess piece
+
+        while self.is_valid_position((x + dx, y + dy)):  # ******Discuss Later****
+            x += dx # calculating possible move
+            y += dy
+
+            piece = self.get_piece_at((x, y)) # info of piece at new calculated move
+
+            if piece: # if there is piece go through condition otherwise add it as valid move
+                if piece.color.color != color: # is selected piece and piece at new position different?
+                    moves.append((x, y)) # the move is valid (possible capture move)
+                break
+
+            moves.append((x,y))
+     
+     return moves # return possible moves filtering out the positions where there is piece of same color
